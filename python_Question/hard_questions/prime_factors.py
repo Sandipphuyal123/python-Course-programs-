ba60bfe3b03271e3264658c7a41c6403a7f282d7
sandipphuyal123 <sandipphuyal24@gmail.com>
2026-07-18 22:50:47 +0545

    added a hard question

---
+++ python_Question/hard_questions/prime_factors.py
@@ -0,0 +1,16 @@
+def prime_factors(n):
+    factors = []
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+# Test
+print(prime_factors(12))
+print(prime_factors(100))
+print(prime_factors(13))
